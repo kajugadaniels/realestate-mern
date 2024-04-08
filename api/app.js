2b67090a0baf43383e9defc9e1b1f0e12cf99637
kajugadaniels <kajugadaniels@gmail.com>
2024-04-08 18:44:04 +0200

    Installation

--- api/app.js
+++ api/app.js
@@ -2,6 +2,10 @@ import express from "express"
 
 const app = express()
 
+app.use("/api/test", (req, res) => {
+    console.log("Testing")
+})
+
 app.listen(8000, () => {
     console.log("Server is running")
 })
