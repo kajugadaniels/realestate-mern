10ddc3676af98f706d278872acda7946c5ae899f
kajugadaniels <kajugadaniels@gmail.com>
2024-04-09 11:35:23 +0200

    Initial prisma & register functionality

--- api/app.js
+++ api/app.js
@@ -1,9 +1,10 @@
 import express from "express"
 import authRouter from './routes/auth.route.js'
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.get("api/auth", authRouter)
+app.use("/api/auth", authRouter);
 
 app.listen(8000, () => {
     console.log("Server is running")
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,40 @@
 import bcrypt from "bcrypt"
+import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
     const { username, email, password } = req.body;
 
-    // Hash password
-    const hashedPassword = await bcrypt.hash(password, 10)
+    if (!username || !email || !password) {
+        return res.status(400).json({ error: 'Username, email, and password are required' });
+    }
 
-    console.log(hashedPassword)
+    try {
+        // Hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        console.log('Password:', password);
+        console.log('Hashed Password:', hashedPassword);
+
+        // CREATE A NEW USER AND SAVE TO DB
+        const newUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: hashedPassword,
+            },
+        });
+
+        console.log('New User:', newUser);
+
+        res.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Failed to create user!' });
+    }
 }
 
-export const login = (req, res) => {
-    console.log("Login endpoints")
+export const login = async (req, res) => {
+    
 }
 
 export const logout = (req, res) => {
