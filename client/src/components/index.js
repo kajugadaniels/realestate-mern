c93e0c25029ffe21e9fbd78a8d37e2df7a20d919
kajugadaniels <kajugadaniels@gmail.com>
2024-04-15 12:54:04 +0200

    Folder

---
+++ client/src/components/index.js
@@ -0,0 +1,21 @@
+import Card from './Card/Card'
+import Chat from './Chat/Chat'
+import Filter from './Filter/Filter'
+import List from './List/List'
+import Map from './Map/Map'
+import Navbar from './Navbar/Navbar'
+import Pin from './Pin/Pin'
+import SearchBar from './SearchBar/SearchBar'
+import Slider from './Slider/Slider'
+
+export {
+    Card,
+    Chat,
+    Filter,
+    List,
+    Map,
+    Navbar,
+    Pin,
+    SearchBar,
+    Slider
+}
---
+++ client/src/routes/index.js
@@ -0,0 +1,21 @@
+import Homepage from './HomePage/HomePage'
+import Layout from './Layout/Layout'
+import ListPage from './ListPage/ListPage'
+import Login from './Login/Login'
+import NewPostPage from './NewPostPage/NewPostPage'
+import ProfilePage from './ProfilePage/ProfilePage'
+import ProfileUpdatePage from './ProfileUpdatePage/ProfileUpdatePage'
+import Register from './Register/Register'
+import SinglePage from './SinglePage/SinglePage'
+
+export {
+    Homepage,
+    Layout,
+    ListPage,
+    Login,
+    NewPostPage,
+    ProfilePage,
+    ProfileUpdatePage,
+    Register,
+    SinglePage,
+}
