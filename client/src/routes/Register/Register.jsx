9c8fd4ea9c81c615f1aee08d44a1595e010caf94
kajugadaniels <kajugadaniels@gmail.com>
2024-04-15 17:31:10 +0200

    Modify register.jsx

--- client/src/routes/Register/Register.jsx
+++ client/src/routes/Register/Register.jsx
@@ -1,15 +1,18 @@
-import axios from "axios"
 import "./Register.scss";
 import { useState } from "react";
+import apiRequest from "../../lib/apiRequest";
 import { Link, useNavigate } from "react-router-dom";
 
 function Register() {
   const [error, setError] = useState("")
+  const [isLoading, setIsLoading] = useState()
 
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault()
+    setError("")
+    setIsLoading(true);
     const formData = new FormData(e.target);
 
     const username = formData.get("username")
@@ -17,7 +20,7 @@ function Register() {
     const password = formData.get("password")
 
     try {
-      const res = await axios.post("http://localhost:8000/api/auth/register", {
+      const res = await apiRequest.post("/auth/register", {
         username,
         email,
         password,
@@ -26,6 +29,8 @@ function Register() {
       navigate("/login")
     } catch (err) {
       setError(err.response.data.message);
+    } finally {
+      setIsLoading(false)
     }
   }
 
@@ -37,7 +42,7 @@ function Register() {
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
-          <button >Register</button>
+          <button disabled={isLoading}>Register</button>
           {error && <span>{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
