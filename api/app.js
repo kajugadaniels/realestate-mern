b589d2cb688328d392571f8d2ed76f4debe25b15
kajugadaniels <kajugadaniels@gmail.com>
2024-04-15 17:12:19 +0200

    Add register user functionality

--- api/app.js
+++ api/app.js
@@ -1,11 +1,16 @@
-import express from "express"
+import express from 'express'
+import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import authRouter from './routes/auth.route.js'
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+}));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 
