4d4ff8431fe7d8cc435a5d09e519772920366c56
kajugadaniels <kajugadaniels@gmail.com>
2024-04-15 14:37:07 +0200

    Stylize Register page

--- client/src/routes/Register/Register.jsx
+++ client/src/routes/Register/Register.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import "./Register.scss";
+import { Link } from "react-router-dom";
 
-const Register = () => {
+function Register() {
   return (
-    <div>Register</div>
-  )
+    <div className="register">
+      <div className="formContainer">
+        <form>
+          <h1>Create an Account</h1>
+          <input name="username" type="text" placeholder="Username" />
+          <input name="email" type="text" placeholder="Email" />
+          <input name="password" type="password" placeholder="Password" />
+          <button >Register</button>
+          <Link to="/login">Do you have an account?</Link>
+        </form>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
