2620433acd493740dfc452b6d6655b619a86d4f9
kajugadaniels <kajugadaniels@gmail.com>
2024-04-15 18:23:22 +0200

    Protect routes functionality

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import authRouter from './routes/auth.route.js'
+import testRouter from './routes/test.route.js'
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
+app.use("/api/test", testRouter);
 
 app.listen(8000, () => {
     console.log("Server is running")
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,84 +1,85 @@
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
+
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
-    const { username, email, password } = req.body;
+  const { username, email, password } = req.body;
 
-    if (!username || !email || !password) {
-        return res.status(400).json({ error: 'Username, email, and password are required' });
-    }
+  try {
+    // HASH THE PASSWORD
 
-    try {
-        // Hash password
-        const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        console.log('Password:', password);
-        console.log('Hashed Password:', hashedPassword);
+    console.log(hashedPassword);
 
-        // CREATE A NEW USER AND SAVE TO DB
-        const newUser = await prisma.user.create({
-            data: {
-                username,
-                email,
-                password: hashedPassword,
-            },
-        });
+    // CREATE A NEW USER AND SAVE TO DB
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword,
+      },
+    });
 
-        console.log('New User:', newUser);
+    console.log(newUser);
 
-        res.status(201).json({ message: "User created successfully" });
-    } catch (error) {
-        console.error('Error:', error);
-        res.status(500).json({ error: 'Failed to create user!' });
-    }
-}
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to create user!" });
+  }
+};
 
 export const login = async (req, res) => {
-    const { username, email, password } = req.body;
+  const { username, password } = req.body;
 
-    try {
-        // Check if user exists
-        const user = await prisma.user.findUnique({
-            where: { username }
-        })
+  try {
+    // CHECK IF THE USER EXISTS
 
-        if (!user) return res.status(401).json({ message: "Invalid credentials" })
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
 
-        // Check if password is correct
-        const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!user) return res.status(400).json({ message: "Invalid Credentials!" });
 
-        if (!isPasswordValid) return res.status(401).json({ message: "Wrong password" })
+    // CHECK IF THE PASSWORD IS CORRECT
 
-        // Generate cookie token and send to the user
-        // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
-        const age = 1000 * 60 * 60 * 24 * 7
+    if (!isPasswordValid)
+      return res.status(400).json({ message: "Invalid Credentials!" });
 
-        const token = jwt.sign(
-            {
-                id:user.id
-            },
-            process.env.JWT_SECRET_KEY,
-            { expiresIn: age }
-        )
+    // GENERATE COOKIE TOKEN AND SEND TO THE USER
 
-        const { password: userPassword, ...userInfo } = user
+    // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
+    const age = 1000 * 60 * 60 * 24 * 7;
 
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: true,
-            maxAge: age
-        })
-        .status(200)
-        .json(userInfo)
-    } catch (error) {
-        console.log(error);
+    const token = jwt.sign(
+      {
+        id: user.id,
+        isAdmin: false,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
 
-        res.status(500).json({ error: "Failed to login user" });
-    }
-}
+    const { password: userPassword, ...userInfo } = user;
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        // secure:true,
+        maxAge: age,
+      })
+      .status(200)
+      .json(userInfo);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to login!" });
+  }
+};
 
 export const logout = (req, res) => {
-    res.clearCookie("token").status(200).json({ message: "Logout Successful" })
-}
+  res.clearCookie("token").status(200).json({ message: "Logout Successful" });
+};
