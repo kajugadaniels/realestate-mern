e7446b979323e9a3c0fb69eb91f58d13b595b5c3
kajugadaniels <kajugadaniels@gmail.com>
2024-04-15 10:37:30 +0200

    Add login functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
@@ -34,7 +35,46 @@ export const register = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    
+    const { username, email, password } = req.body;
+
+    try {
+        // Check if user exists
+        const user = await prisma.user.findUnique({
+            where: { username }
+        })
+
+        if (!user) return res.status(401).json({ message: "Invalid credentials" })
+
+        // Check if password is correct
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) return res.status(401).json({ message: "Wrong password" })
+
+        // Generate cookie token and send to the user
+        // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
+
+        const age = 1000 * 60 * 60 * 24 * 7
+
+        const token = jwt.sign(
+            {
+                id:user.id
+            },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: age }
+        )
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: true,
+            maxAge: age
+        })
+        .status(200)
+        .json({ message: "Login Successful" })
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({ error: "Failed to login user" });
+    }
 }
 
 export const logout = (req, res) => {
