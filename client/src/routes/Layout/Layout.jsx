67f86e1601a4a8507aa197f34a537cd028eaa9c1
kajugadaniels <kajugadaniels@gmail.com>
2024-04-15 14:20:04 +0200

    Stylize navbar

--- client/src/routes/Layout/Layout.jsx
+++ client/src/routes/Layout/Layout.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { Navbar } from '../../components'
-import { Outlet } from 'react-router-dom'
+import "./Layout.scss";
+import { Navbar } from "../../components/"
+import { Outlet } from "react-router-dom";
 
-const Layout = () => {
+function Layout() {
   return (
-    <div>
-      <Navbar />
-      <Outlet />
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <Outlet/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
