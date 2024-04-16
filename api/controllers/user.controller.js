b48662efe64b3e4eacfcb3c3cb4c188dfebd87aa
kajugadaniels <kajugadaniels@gmail.com>
2024-04-16 17:12:58 +0200

    Get single user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -11,8 +11,13 @@ export const getUsers = async (req, res) => {
 }
 
 export const getUser = async (req, res) => {
+    const id = req.params.id;
+
     try {
-        
+        const user = await prisma.user.findUnique({
+          where: { id }
+        });
+        res.status(200).json(user)
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: "Failed to get user!" })
