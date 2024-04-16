8bf8b4099e61c506cfc0015b0b5805f2b1eebef5
kajugadaniels <kajugadaniels@gmail.com>
2024-04-16 17:33:40 +0200

    Add update user profile functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import prisma from "../lib/prisma.js";
+import bcrypt from "bcrypt";
 
 export const getUsers = async (req, res) => {
     try {
@@ -15,7 +16,7 @@ export const getUser = async (req, res) => {
 
     try {
         const user = await prisma.user.findUnique({
-          where: { id }
+            where: { id }
         });
         res.status(200).json(user)
     } catch (error) {
@@ -25,8 +26,31 @@ export const getUser = async (req, res) => {
 }
 
 export const updateUser = async (req, res) => {
+    const id = req.params.id;
+    const tokenUserId = req.userId;
+    const { password, avatar, ...inputs } = req.body;
+
+    if (id !== tokenUserId) {
+        return res.status(403).json({ message: "Not Authorized" })
+    }
+
+    let updatePassword = null;
+
     try {
-        
+        if (password) {
+            updatePassword = await bcrypt.hash(password, 10)
+        }
+
+        const updateUser = await prisma.user.update({
+            where: { id },
+            data: {
+                ...inputs,
+                ...(updatePassword && { password: updatePassword }),
+                ...(avatar && { avatar })
+            },
+        });
+
+        res.status(200).json(updateUser)
     } catch (error) {
         console.log(error)
         res.status(500).json({ message: "Failed to update user!" })
