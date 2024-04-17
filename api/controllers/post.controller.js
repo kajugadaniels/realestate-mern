157701e62e7769297ce21edbd09a6c8b933f2d81
kajugadaniels <kajugadaniels@gmail.com>
2024-04-17 16:59:46 +0200

    User add post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -3,35 +3,42 @@ import jwt from "jsonwebtoken";
 
 export const getPosts = async (req, res) => {
     try {
-        
+        const posts = await prisma.post.findMany()
+
+        res.status(200).json(posts)
     } catch (error) {
         console.log(error)
+        res.status(500).json({ message: "Failed to get posts" })
     }
 }
+
 export const getPost = async (req, res) => {
     try {
-        
+
     } catch (error) {
         console.log(error)
     }
 }
+
 export const addPost = async (req, res) => {
     try {
-        
+
     } catch (error) {
         console.log(error)
     }
 }
+
 export const updatePost = async (req, res) => {
     try {
-        
+
     } catch (error) {
         console.log(error)
     }
 }
+
 export const deletePost = async (req, res) => {
     try {
-        
+
     } catch (error) {
         console.log(error)
     }
