5f355fd98019b091a7ba313413e96a0155f04653
kajugadaniels <kajugadaniels@gmail.com>
2024-04-17 15:36:09 +0200

    Configure post route & controller

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser'
 import authRouter from './routes/auth.route.js'
 import testRouter from './routes/test.route.js'
 import userRouter from './routes/user.route.js'
+import postRouter from './routes/post.route.js'
 
 const app = express();
 
@@ -17,6 +18,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRouter);
 app.use("/api/test", testRouter);
 app.use("/api/users", userRouter);
+app.use("/api/post", postRouter);
 
 app.listen(8000, () => {
     console.log("Server is running")
---
+++ api/controllers/post.controller.js
@@ -0,0 +1,38 @@
+import prisma from "../lib/prisma.js";
+import jwt from "jsonwebtoken";
+
+export const getPosts = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const getPost = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const addPost = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const updatePost = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
+export const deletePost = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error)
+    }
+}
