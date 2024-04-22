3333a1803308319d563c98cf428274de26574c67
kajugadaniels <kajugadaniels@gmail.com>
2024-04-22 22:14:20 +0530

    Fetch single data from db using react-router-dom loader

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HomePage, ListPage, Login, NewPostPage, ProfilePage, ProfileUpdatePage, Register, SinglePage } from './routes'
 import { Layout, RequireAuth } from "./routes/Layout/Layout";
+import { singlePageLoader } from "./lib/loaders";
 
 function App() {
   const router = createBrowserRouter([
@@ -18,7 +19,8 @@ function App() {
         },
         {
           path:"/:id",
-          element:<SinglePage />
+          element:<SinglePage />,
+          loader: singlePageLoader,
         },
         {
           path:"/login",
